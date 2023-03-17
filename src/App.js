23b6b74deefc74669cef798dbba1204d7e375bcb
Dhruv <77236182+thisDhruv@users.noreply.github.com>
2023-03-18 00:42:37 +0530

    helpBox inside nav component to avoid rerender

--- src/App.js
+++ src/App.js
@@ -1,19 +1,14 @@
 import { useState } from "react";
 import "./App.css";
 import { FileUpload } from "./Components/FileUpload";
-import { HelpSteps } from "./Components/HelpSteps";
 import { NavBar } from "./Components/NavBar";
 import { Stats } from "./Components/Stats";
 function App() {
   const [fileContent, setFileContent] = useState("None");
-  const [helpOn, setHelp] = useState(false);
-  function helpClicked() {
-    setHelp(!helpOn);
-  }
+
   return (
     <>
-      <NavBar helpClicked={helpClicked} />
-      <HelpSteps helpOn={helpOn}/>
+      <NavBar/>
       <div className="container flex flex-row mx-auto w-full">
         <FileUpload setFileContent={setFileContent} />
       </div>
--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -1,8 +1,12 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export const NavBar = (props) => {
+  const [helpOn, setHelp] = useState(false);
+  function helpClicked() {
+    setHelp(!helpOn);
+  }
   return (
-      
+<>
 <nav className="bg-white border-gray-200 px-2 sm:px-4 py-2.5 rounded dark:bg-gray-900">
   <div className="container flex flex-wrap items-center justify-between mx-auto">
     <a href="/" className="flex items-center">
@@ -17,7 +21,7 @@ export const NavBar = (props) => {
     <button
         type="button"
         id="howtobutton"
-        onClick={props.helpClicked}
+        onClick={helpClicked}
         className="text-purple-700 hover:text-white border border-purple-700 hover:bg-purple-800 focus:ring-4 focus:outline-none focus:ring-purple-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 dark:border-purple-400 dark:text-purple-400 dark:hover:text-white dark:hover:bg-purple-500 dark:focus:ring-purple-900"
       >
         Help
@@ -25,6 +29,47 @@ export const NavBar = (props) => {
     </div>
   </div>
 </nav>
+ {helpOn && (
+  <ol className="items-center w-full space-y-4 mb-4 sm:flex sm:space-x-8 sm:space-y-0">
+    <li className="flex items-center text-black-600 dark:text-black-500 space-x-2.5">
+      <span className="flex items-center justify-center w-8 h-8 border border-blue-600 rounded-full shrink-0 dark:border-blue-500">
+        1
+      </span>
+      <span>
+        <h3 className="font-medium leading-tight">Export Chat</h3>
+        <p className="text-sm">In WhatsApp Chat: Three Dots → More → Export chat → Without media  </p>
+      </span>
+    </li>
+    <li className="flex items-center text-black-500 dark:text-gray-400 space-x-2.5">
+      <span className="flex items-center justify-center w-8 h-8 border border-gray-500 rounded-full shrink-0 dark:border-gray-400">
+        2
+      </span>
+      <span>
+        <h3 className="font-medium leading-tight">Mail Yourself</h3>
+        <p className="text-sm">Select Gmail to mail the exported file to your email address</p>
+      </span>
+    </li>
+    <li className="flex items-center text-black-500 dark:text-gray-400 space-x-2.5">
+      <span className="flex items-center justify-center w-8 h-8 border border-gray-500 rounded-full shrink-0 dark:border-gray-400">
+        3
+      </span>
+      <span>
+        <h3 className="font-medium leading-tight">Download exported file on your machine</h3>
+        <p className="text-sm">Download the file from your mail</p>
+      </span>
+    </li>
+    <li className="flex items-center text-black-500 dark:text-gray-400 space-x-2.5">
+      <span className="flex items-center justify-center w-8 h-8 border border-gray-500 rounded-full shrink-0 dark:border-gray-400">
+        4
+      </span>
+      <span>
+        <h3 className="font-medium leading-tight">Select the file</h3>
+        <p className="text-sm">Select the downloaded file by clicking the below box</p>
+      </span>
+    </li>
+  </ol>
+)}
+</>
 
   )
 }
